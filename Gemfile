3c343fa9dd534b601f43ad3ec5e67e4fa7ea12f5
yutaro-kugishima <yocw6389@yahoo.co.jp>
2020-12-11 15:57:57 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -59,3 +59,6 @@ gem 'devise'
 group :production do
   gem 'rails_12factor'
 end
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -39,7 +39,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :text, :picture).merge(user_id: current_user.id)
   end
 
   def set_tweet
