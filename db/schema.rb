e8f073c49fb9d1f8ab337a42d60726426f44c01e
yutaro-kugishima <yocw6389@yahoo.co.jp>
2020-12-19 20:34:13 +0900

    モデルの実装

--- db/schema.rb
+++ db/schema.rb
@@ -41,21 +41,6 @@ ActiveRecord::Schema.define(version: 2020_12_11_063452) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tweet_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "tweet_id"
-    t.bigint "tag_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tag_id"], name: "index_tweet_tag_relations_on_tag_id"
-    t.index ["tweet_id"], name: "index_tweet_tag_relations_on_tweet_id"
-  end
-
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text"
     t.text "image"
@@ -67,17 +52,22 @@ ActiveRecord::Schema.define(version: 2020_12_11_063452) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "birth_place"
+    t.string "residence"
+    t.string "blood_type"
+    t.date "birth_date"
+    t.integer "age"
+    t.string "profession"
+    t.string "hobby"
+    t.string "nickname"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "tweet_tag_relations", "tags"
-  add_foreign_key "tweet_tag_relations", "tweets"
 end
