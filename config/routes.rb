0d625741f002e683bf5b47751fbf44fad58e054a
yutaro-kugishima <yocw6389@yahoo.co.jp>
2021-01-02 22:00:51 +0900

    ルーティング、ビューの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'users#index'
   resources :users do
-  # get 'users/search'
+  get 'users/search'
     # collection do
     #   get 'search'
     # end
