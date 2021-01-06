ecfacc3898a47b1581ef81032601aea50918f205
yutaro-kugishima <yocw6389@yahoo.co.jp>
2021-01-07 08:13:55 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
-class MessagesController < ApplicationController
-  def new
-    @messages = Message.all
-    @message = Message.new
-  end
+# class MessagesController < ApplicationController
+#   def new
+#     @messages = Message.all
+#     @message = Message.new
+#   end
 
-  def create
-    @message = Message.new(text: params[:message][:text])
-  end
-end
+#   def create
+#     @message = Message.new(text: params[:message][:text])
+#   end
+# end
