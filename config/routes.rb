4f54bbfafbbb6b36651713674607e88beed2229a
yutaro-kugishima <yocw6389@yahoo.co.jp>
2021-01-29 22:09:14 +0900

    その他の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
   mount ActionCable.server => '/cable'
   get 'chats/index'
   get 'chats/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'users#index'
+  # root to: 'rooms#show'
   get 'users/search', to: 'users#search'
   resources :users do
     resources :chats
