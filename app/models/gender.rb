f7890927824ebcf210188cb71136f00de383f103
yutaro-kugishima <yocw6389@yahoo.co.jp>
2021-02-02 22:30:25 +0900

    検索機能の実装（性別モデルの編集）

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,12 +1,9 @@
 class Gender < ActiveHash::Base
 
   self.data = [
-              {id: 0, name: '--'}, {id: 1, name: '男'}, {id: 2, name: '女'}, 
+              {id: 1, name: '男'}, {id: 2, name: '女'}, 
             
             ]
 
-            # def ransackable_attributes(auth_object = nil)
-            #   column_names + _ransackers.keys
-            # end
 end
 
