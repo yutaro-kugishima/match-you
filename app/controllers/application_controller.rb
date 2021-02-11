dad0e8b27c4e2ab5d3e8725f591b4687e22ffdd1
yutaro-kugishima <yocw6389@yahoo.co.jp>
2021-02-11 18:25:45 +0900

    basic認証の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_place_id, :residence_id, :blood_type_id, :birth_date, :age, :profession, :hobby, :text, :gender_id, :picture])
-    
-  end
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
-    end
   end
 end
